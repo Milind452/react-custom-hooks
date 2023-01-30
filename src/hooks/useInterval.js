224858ce2835df3184f64e1ce03608b65d243722
Milind452 <mohapatra.milind@gmail.com>
2023-01-30 17:41:33 +0530

    Add useInterval

---
+++ src/hooks/useInterval.js
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+
+function useInterval(callback, delay) {
+    const callbackRef = useRef();
+
+    useEffect(() => {
+        callbackRef.current = callback;
+    }, [callback]);
+
+    useEffect(() => {
+        if (delay == null) return;
+
+        const intervalID = setInterval(() => {
+            callbackRef.current();
+        }, delay);
+
+        return () => clearInterval(intervalID);
+    }, [delay]);
+}
+
+export default useInterval;
