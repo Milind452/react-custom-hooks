9c94f151bc35a38a9aa7fba94a83d21c2b265047
Milind452 <mohapatra.milind@gmail.com>
2023-01-30 20:11:53 +0530

    Configure routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// Hooks
+import UseFetch from './Components/UseFetch';
+import UseMap from './Components/UseMap';
+import UseLocalStorage from './Components/UseLocalStorage';
+import UseInterval from './Components/UseInterval';
+import UseStateWithHistory from './Components/UseStateWithHistory';
+import UseWindowSize from './Components/UseWindowSize';
+
+import Home from './Components/Home';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const hooks = [
+    {'name': 'useFetch', 'component': <UseFetch/>}, 
+    {'name': 'useLocalStorage', 'component': <UseLocalStorage/>}, 
+    {'name': 'useInterval', 'component': <UseInterval/>}, 
+    {'name': 'useStateWithHistory', 'component': <UseStateWithHistory/>}, 
+    {'name': 'useMap', 'component': <UseMap/>}, 
+    {'name': 'useWindowSize', 'component': <UseWindowSize/>}
+  ];
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {
+          hooks.map(({name, component}) => {
+            console.log(name, component);
+            return <Route key={name} path={name} element={component} />;
+          })
+        }
+      </Routes>
+    </BrowserRouter>
   )
 }
 
