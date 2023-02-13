2c2d1e27dfc048eb8ff2ba5012febda492e0b2bd
Milind452 <mohapatra.milind@gmail.com>
2023-02-13 21:33:22 +0530

    Fix key error in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,19 @@
-import {Link} from 'react-router-dom';
-import reactLogo from '../assets/react.svg'
+import { Link } from "react-router-dom";
+import reactLogo from "../assets/react.svg";
 
-export default function Navbar({hooks}) {
+export default function Navbar({ hooks }) {
     return (
         <>
             <img src={reactLogo} className="logo react" alt="React logo" />
-            <ul className='menu'>
-                {
-                    hooks.map(hook => (
-                        <li className='menu-item'>
-                            <Link key={hook} to={`/${hook}`} className='link'>{hook}</Link>
-                        </li>
-                    ))
-                }
+            <ul className="menu">
+                {hooks.map((hook) => (
+                    <li key={hook} className="menu-item">
+                        <Link to={`/${hook}`} className="link">
+                            {hook}
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </>
-    )
+    );
 }
