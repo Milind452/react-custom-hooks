667527ced27d9387814e90b6531e1e1704977275
Milind452 <mohapatra.milind@gmail.com>
2023-02-13 19:02:44 +0530

    Add all hooks to navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,19 @@
 import {Link} from 'react-router-dom';
+import reactLogo from '../assets/react.svg'
 
-export default function Navbar() {
-    return <h2>Navbar</h2>;
+export default function Navbar({hooks}) {
+    return (
+        <>
+            <img src={reactLogo} className="logo react" alt="React logo" />
+            <ul className='menu'>
+                {
+                    hooks.map(hook => (
+                        <li className='menu-item'>
+                            <Link key={hook} to={`/${hook}`} className='link'>{hook}</Link>
+                        </li>
+                    ))
+                }
+            </ul>
+        </>
+    )
 }
