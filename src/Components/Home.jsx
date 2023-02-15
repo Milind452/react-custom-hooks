1a11179700533de342317ff167d29682b5974db7
Milind452 <mohapatra.milind@gmail.com>
2023-02-15 08:50:59 +0530

    Add home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,3 +1,19 @@
 export default function Home() {
-    return <h2>Home</h2>
+    return (
+        <article style={{ height: "100vh" }}>
+            <h2>Home</h2>
+            <p>
+                This a collection of custom hooks for react to make our lives
+                easier as developers. I'll keep adding to this collection as I
+                create more hooks.
+            </p>
+            <p>Feel free to use these in your own projects.</p>
+            <footer>
+                <p>
+                    For any suggestions, you can contact me via{" "}
+                    <a href="mailto:mohapatra.milind@gmail.com">email</a>
+                </p>
+            </footer>
+        </article>
+    );
 }
