0c97f69923c943e48140293a2f3164337ab398fc
Milind452 <mohapatra.milind@gmail.com>
2023-02-16 19:24:46 +0530

    Highlight selected nav link

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,31 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 import reactLogo from "../assets/react.svg";
 
 export default function Navbar({ hooks }) {
+    const menu = useRef();
+    function handleClick(e) {
+        const menuItems = menu.current.childNodes;
+        menuItems.forEach((item) => {
+            item.childNodes[0].classList.remove("selected-nav-link");
+        });
+        const link = e.target;
+        link.classList.add("selected-nav-link");
+    }
+
     return (
         <>
             <Link to="/">
                 <img src={reactLogo} className="logo react" alt="React logo" />
             </Link>
-            <ul className="menu">
+            <ul className="menu" ref={menu}>
                 {hooks.map((hook) => (
                     <li key={hook} className="menu-item">
-                        <Link to={`/${hook}`} className="link">
+                        <Link
+                            to={`/${hook}`}
+                            className="link"
+                            onClick={handleClick}
+                        >
                             {hook}
                         </Link>
                     </li>
